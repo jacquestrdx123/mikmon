a701217514d6b43f38739b3cea9dc3cf8de4e191
jacquestredoux <jacques.trdx@gmail.com>
2021-05-07 11:26:49 +0200

    Fixed Providers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,84 +36,32 @@
                                             </div>
                                             <div class="mt-3">
                                                 <ul class="list list-inline">
-                                                    <li class="d-flex justify-content-between">
-                                                        <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
-                                                            <div class="ml-2">
-                                                                <h6 class="mb-0">Kickoff meeting</h6>
-                                                                <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                                                                    <div><i class="fa fa-calendar-o"></i><span class="ml-2">22 May 2020 11:30 PM</span></div>
-                                                                    <div class="ml-3"><i class="fa fa-clock-o"></i><span class="ml-2">6h</span></div>
+                                                    @foreach($offline_devices as $offline_device)
+                                                        <li>
+                                                            <card>
+
+                                                            </card>
+                                                        </li>
+                                                        <li class="d-flex justify-content-between">
+                                                            <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
+                                                                <div class="ml-2">
+                                                                    <h6 class="mb-0">{!! $device->location->description !!}</h6>
+                                                                    <div class="d-flex flex-row mt-1 text-black-50 date-time">
+                                                                        <a href="/device/{!! $offline_device->id !!}">
+                                                                            {!! $offline_device->description !!}
+                                                                        </a>
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <div class="d-flex flex-column mr-2">
-                                                                <div class="profile-image"><img class="rounded-circle" src="https://i.imgur.com/xbxOs06.jpg" width="30"><img class="rounded-circle" src="https://i.imgur.com/KIJewDa.jpg" width="30"><img class="rounded-circle" src="https://i.imgur.com/wwd9uNI.jpg" width="30"></div><span class="date-time">11/4/2020 12:55</span>
-                                                            </div> <i class="fa fa-ellipsis-h"></i>
-                                                        </div>
-                                                    </li>
-                                                    <li class="d-flex justify-content-between">
-                                                        <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
-                                                            <div class="ml-2">
-                                                                <h6 class="mb-0">User Interview</h6>
-                                                                <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                                                                    <div><i class="fa fa-calendar-o"></i><span class="ml-2">25 May 2020 12:34 AM</span></div>
-                                                                    <div class="ml-3"><i class="fa fa-clock-o"></i><span class="ml-2">4h</span></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <div class="d-flex flex-column mr-2">
-                                                                <div class="profile-image"><img class="rounded-circle" src="https://i.imgur.com/xbxOs06.jpg" width="30"><img class="rounded-circle" src="https://i.imgur.com/wwd9uNI.jpg" width="30"></div><span class="date-time">12/5/2020 12:55</span>
-                                                            </div><i class="fa fa-ellipsis-h"></i>
-                                                        </div>
-                                                    </li>
-                                                    <li class="d-flex justify-content-between">
-                                                        <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
-                                                            <div class="ml-2">
-                                                                <h6 class="mb-0">Prototyping</h6>
-                                                                <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                                                                    <div><i class="fa fa-calendar-o"></i><span class="ml-2">17 May 2020 1:34 PM</span></div>
-                                                                    <div class="ml-3"><i class="fa fa-clock-o"></i><span class="ml-2">6h</span></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <div class="d-flex flex-column mr-2">
-                                                                <div class="profile-image"><img class="rounded-circle" src="https://i.imgur.com/xbxOs06.jpg" width="30"><img class="rounded-circle" src="https://i.imgur.com/KIJewDa.jpg" width="30"></div><span class="date-time">16/4/2020 1:55</span>
-                                                            </div><i class="fa fa-ellipsis-h"></i>
-                                                        </div>
-                                                    </li>
-                                                    <li class="d-flex justify-content-between">
-                                                        <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
-                                                            <div class="ml-2">
-                                                                <h6 class="mb-0">Call with client</h6>
-                                                                <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                                                                    <div><i class="fa fa-calendar-o"></i><span class="ml-2">12 May 2020 12:35 AM</span></div>
-                                                                    <div class="ml-3"><i class="fa fa-clock-o"></i><span class="ml-2">6h</span></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <div class="d-flex flex-column mr-2">
-                                                                <div class="profile-image"><img class="rounded-circle" src="https://i.imgur.com/wwd9uNI.jpg" width="30"></div><span class="date-time">11/4/2020 12:55</span>
-                                                            </div><i class="fa fa-ellipsis-h"></i>
-                                                        </div>
-                                                    </li>
+                                                        </li>
+                                                    @endforeach
+
                                                 </ul>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                    @foreach($offline_devices as $offline_device)
-                                        <li>
-                                            <card>
-                                                <a href="/device/{!! $offline_device->id !!}">
-                                                    {!! $offline_device->description !!}
-                                                </a>
-                                            </card>
-                                        </li>
-                                    @endforeach
+
 
                             </div>
                         </div>
