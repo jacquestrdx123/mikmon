63e3f68e80a54f7b871dae5e93ddf58aa1dbdbbc
jacquestredoux <jacques.trdx@gmail.com>
2021-05-11 11:29:46 +0200

    Fixed Providers

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,18 +2,41 @@
 
 namespace App\Providers;
 
-use Horizon;
-use Illuminate\Support\ServiceProvider;
+use App\User;
+use Laravel\Horizon\Horizon;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
 
-class HorizonServiceProvider extends ServiceProvider
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        if (! app()->environment('local')) {
-            Horizon::auth(fn ($request) => auth()->check() && auth()->user()->isAdmin());
-        }
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        Horizon::routeMailNotificationsTo('jacques.trdx@gmail.com');
+
+        //Horizon::routeSlackNotificationsTo(config('services.slack.webhook_url'), '#horizon-demo');
+
+        //Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewHorizon', function (User $user) {
+            return true;
+        });
     }
 }
