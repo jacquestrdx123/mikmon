11dd96ba70c62bf2c6556003eb6e702ae8fd8f04
jacquestredoux <jacques.trdx@gmail.com>
2021-05-11 12:00:30 +0200

    Fixed Providers

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
 
         /*
          * Application Service Providers...
-         */ 
+         */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
