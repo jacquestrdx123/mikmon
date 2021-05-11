1e24e99d0a2b5aa09487a567bda67388b5152177
jacquestredoux <jacques.trdx@gmail.com>
2021-05-11 10:07:04 +0200

    Fixed Providers

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -100,6 +100,7 @@ class Device extends Model
         $array = array();
         foreach($devices as $device) {
             if ($device->status == "4") {
+                dd($device);
                 $rrdFile = config('rrd.storage_path')."/pings/". trim($device->ip) . ".rrd";
                 $result = \rrd_fetch($rrdFile, array(config('rrd.ds'), "--resolution", config("rrd.step"), "--start", (time() - 86400), "--end", (time() - 350)));
                 if (isset($result['data'])) {
