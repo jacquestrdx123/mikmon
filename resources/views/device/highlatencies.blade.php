75d356457c40c57d4ae7f41891ba09e6740e32de
jacquestredoux <jacques.trdx@gmail.com>
2021-05-12 10:16:56 +0200

    Fixed Providers

--- resources/views/device/highlatencies.blade.php
+++ resources/views/device/highlatencies.blade.php
@@ -15,20 +15,22 @@
                                 <button class="btn btn-default collapsible">
                                         <strong> Latency Events for {!! $index !!} : {{sizeof($array)}} :  &nbsp &nbsp </strong>
                                 </button>
-                                <table class ="table table-bordered table-hover">
-                                    @foreach($array as $key => $latency)
-                                        @foreach($latency as $value)
-                                            <tr>
-                                                <td>{!! $key !!}</td>
-                                                <td>{!! $value['url'] !!}</td>
-                                                <td>{!! $value['type'] !!}</td>
-                                                <td>{!! $value['year'] !!}</td>
-                                                <td>{!! $value['ip'] !!}</td>
-                                                <td>{!! $value['value'] !!}</td>
-                                            </tr>
+                                <div>
+                                    <table class ="table table-bordered table-hover">
+                                        @foreach($array as $key => $latency)
+                                            @foreach($latency as $value)
+                                                <tr>
+                                                    <td>{!! $key !!}</td>
+                                                    <td>{!! $value['url'] !!}</td>
+                                                    <td>{!! $value['type'] !!}</td>
+                                                    <td>{!! $value['year'] !!}</td>
+                                                    <td>{!! $value['ip'] !!}</td>
+                                                    <td>{!! $value['value'] !!}</td>
+                                                </tr>
+                                            @endforeach
                                         @endforeach
-                                    @endforeach
-                                </table>
+                                    </table>
+                                </div>
                             @endforeach
                         </div>
                     </div>
