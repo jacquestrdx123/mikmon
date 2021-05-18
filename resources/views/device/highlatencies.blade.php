c876b68dd138e1ea583aa91f39b414c532117df4
jacquestredoux <jacques.trdx@gmail.com>
2021-05-18 09:28:29 +0200

    Fixed Providers

--- resources/views/device/highlatencies.blade.php
+++ resources/views/device/highlatencies.blade.php
@@ -19,7 +19,7 @@
                                     <ul class="nav navbar-default">
                                     <li>
                                         <table class ="table table-bordered table-hover">
-                                            @if(sizeof(($array) > 10))
+                                            @if(sizeof(current($array) > 10))
                                                 @foreach($array as $key => $latency)
                                                     @foreach($latency as $value)
                                                         <tr style="color:red">
