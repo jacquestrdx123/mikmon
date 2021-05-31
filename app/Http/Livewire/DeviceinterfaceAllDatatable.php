7b7a96be2761135d905a622d154363e232d767fe
jacquestredoux <jacques.trdx@gmail.com>
2021-05-31 10:08:12 +0200

    Fixed Providers

--- app/Http/Livewire/DeviceinterfaceAllDatatable.php
+++ app/Http/Livewire/DeviceinterfaceAllDatatable.php
@@ -21,7 +21,7 @@ class DeviceinterfaceAllDatatable extends LivewireDatatable
 
     public function builder()
     {
-        return Deviceinterface::query()->leftJoin('devices', 'devices.id', 'deviceinterfaces.device_id')->orderBy('txspeed','DESC')->limit('20');
+        return Deviceinterface::query()->leftJoin('devices', 'devices.id', 'deviceinterfaces.device_id')->where('deviceinterfaces.name','like','%T%')->orderBy('txspeed','DESC')->get();
     }
 
     public function columns()
