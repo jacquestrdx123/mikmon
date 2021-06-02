a77ea928f28dc9a71ccf9dc630d7909e5d02e237
jacquestredoux <jacques.trdx@gmail.com>
2021-06-02 11:26:02 +0200

    Fixed Providers

--- app/Libraries/InterfaceLibrary.php
+++ app/Libraries/InterfaceLibrary.php
@@ -523,6 +523,7 @@ class InterfaceLibrary
                 $rrdFile = config('rrd.storage_path') . trim($interface->device_id) . "/interfaces/" . trim($interface->default_name) . ".rrd";
                 $result = rrd_fetch($rrdFile, array('AVERAGE', "--resolution" , '300', "--start", (time() - 5000), "--end", time() - 300));
                 if($result){
+                    dd($result);
                     foreach ($result["data"]["rxvalue"] as $key => $value) {
                         $labels[] = $key;
                     }
