5ea4d41fd7e5358eea8bc869c74255d648e5e415
jacquestredoux <jacques.trdx@gmail.com>
2021-06-21 10:08:23 +0200

    Fixed Providers

--- app/Libraries/MikrotikLibrary.php
+++ app/Libraries/MikrotikLibrary.php
@@ -377,8 +377,9 @@ class MikrotikLibrary
             $default_gateway->disabled = $disabled;
             $default_gateway->type = "Internet";
             $default_gateway->save();
-
+            var_dump($default_gateway);
         }
+        dd("DINE");
     }
 
 
