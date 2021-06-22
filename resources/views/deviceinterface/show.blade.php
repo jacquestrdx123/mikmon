6389331169c01b5a2ce6ba51730c0c13d96d2323
jacquestredoux <jacques.trdx@gmail.com>
2021-06-22 09:14:18 +0200

    Fixed Providers

--- resources/views/deviceinterface/show.blade.php
+++ resources/views/deviceinterface/show.blade.php
@@ -7,12 +7,10 @@
     <div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             {!! $throughputChart->container() !!}
-            {!! $interfaceErrorsChart->container() !!}
             {!! $interfaceUptimeChart->container() !!}
             @push('scripts')
                 <script src="{{ $interfaceErrorsChart->cdn() }}"></script>
                 {{ $throughputChart->script() }}
-                {{ $interfaceErrorsChart->script() }}
                 {{ $interfaceUptimeChart->script() }}
             @endpush
         </div>
